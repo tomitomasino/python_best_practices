44cd1046aba81360904f5bbf5dfc17bfd1494315
Tomek Rucinski <tomasz.rucinski@gmail.com>
2025-03-05 16:59:07 +0100

    test pylint

---
+++ tests/test_data_exporter_fail.py
@@ -0,0 +1,58 @@
+'''
+This file contains the unit tests for the DataExporter class that will fail - for testing workflow.
+'''
+import unittest
+import os
+import json
+import pytest
+from src.utils.exporter import DataExporter
+
+class TestDataExporterFailing(unittest.TestCase):
+    def setUp(self):
+        self.filename = "test_data_failing.json"
+        self.exporter = DataExporter(self.filename)
+        self.test_data = {"name": "Test User", "age": 99, "city": "Test City"}
+
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def test_export_empty_data_fails(self):
+        empty_data = {}
+        result = self.exporter.export_data(empty_data)
+        self.assertTrue(result)
+
+        with open(self.filename, "r", encoding="utf-8") as f:
+            loaded_data = json.load(f)
+        # This assertion will intentionally fail
+        self.assertEqual(loaded_data, {"non-empty": "data"})
+
+    def test_export_nested_data_fails(self):
+        nested_data = {
+            "user": {
+                "name": "Test User",
+                "details": {"age": 30}
+            }
+        }
+        # This will fail because we expect an exception that won't be raised
+        with pytest.raises(ValueError):
+            self.exporter.export_data(nested_data)
+
+    def test_file_content_mismatch(self):
+        self.exporter.export_data(self.test_data)
+
+        with open(self.filename, "r", encoding="utf-8") as f:
+            loaded_data = json.load(f)
+        # This assertion will intentionally fail
+        self.assertNotEqual(loaded_data, self.test_data)
+
+    def test_invalid_json_structure(self):
+        invalid_data = {"key": float('inf')}  # JSON cannot serialize infinity
+        # This should fail because we expect different exception
+        with pytest.raises(ValueError):
+            self.exporter.export_data(invalid_data)
+
+    def test_null_data_handling(self):
+        # This will fail because None is not a valid input
+        result = self.exporter.export_data(None)
+        self.assertTrue(result)
