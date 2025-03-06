cd7445745120161c1b6f5c88ad63e0e914d7bab1
Tomek Rucinski <tomasz.rucinski@gmail.com>
2025-03-06 13:49:29 +0100

    fixes

--- src/core/database.py
+++ src/core/database.py
@@ -1,12 +1,11 @@
-from dotenv import load_dotenv
 import os
-
-# Load environment variables from .env file
-load_dotenv()
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+# Load environment variables from .env file
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
