fbb17efdd7f1a92a8216666ea3b33c021367b683
Tomek Rucinski <tomasz.rucinski@gmail.com>
2025-03-24 13:08:51 +0100

    Azure logic app emails

--- infra/main.tf
+++ infra/main.tf
@@ -98,6 +98,29 @@ BODY
   ]
 }
 
+# MS Teams Action using HTTP Webhook
+resource "azurerm_logic_app_action_http" "send_teams_notification" {
+  name         = "send-teams-notification"
+  logic_app_id = azurerm_logic_app_workflow.email_logic_app.id
+
+  method = "POST"
+  url    = "https://outlook.office.com/webhook/YOUR_TEAMS_WEBHOOK_URL"
+
+  headers = {
+    "Content-Type" = "application/json"
+  }
+
+  body = <<EOT
+{
+  "text": "An email was sent to @{triggerBody()?['to']} with the subject: @{triggerBody()?['subject']}"
+}
+EOT
+
+  depends_on = [
+    azurerm_logic_app_action_custom.send_email
+  ]
+}
+
 # Output the Logic App HTTP endpoint URL
 output "logic_app_trigger_url" {
   value       = azurerm_logic_app_trigger_http_request.email_trigger.callback_url
